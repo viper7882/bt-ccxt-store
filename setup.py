799df9c008ec98fdaae898be9c1f077bd70ba862
KW Lam <klwhot7880@yahoo.co.uk>
2022-12-30 00:59:37 +0800

    Version: 2022.12.30. Added UT to test out execute of BT_CCXT_Account_or_Store. Added exclusive dump on position, CCXT order and order for each order that got executed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
    name='bt_ccxt_store',
-   version='2022.12.27',
+   version='2022.12.30',
    description='A fork of Ed Bartosh\'s CCXT Store Work with some additions',
    url='https://github.com/viper7882/bt-ccxt-store',
    author='Dave Vallance',
